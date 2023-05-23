66fd5dc4899b14772afc67ee16edf8c3d1404be7
Scott <sharding.engineer@gmail.com>
2023-05-23 07:54:57 -0400

    add struct for Plot

--- ctdplot.pl
+++ ctdplot.pl
@@ -23,20 +23,29 @@ foreach my $arg (@ARGV) {
     $index++;
 }
 
-struct Instrument => {
+struct( Instrument => {
         name => '$',
         number => '$',
         quantity_measured => '$',
         units => '$',
-};
+});
+
+struct( Plot => {
+        station => '$',
+        x_instrument => 'Instrument',
+        y_instrument => 'Instrument',
+        x_values => '@',
+        y_values => '@',
+});
+
 
 my @instruments;
 my $instruments = [];
 my @cnv_info;
-my @all_cnv_info;
 my @cnv_files_selected = ();
 my $x_axis;
 my $y_axis;
+my @plots=();
 
 get '/' => sub ($c) {
   #get entire list of cnv files from data dir for user to select from
@@ -52,6 +61,7 @@ get '/' => sub ($c) {
       #returns array of structs
       @instruments = $c->instr_list->get($cnv_fullpath_name);
   }
+
   #get list of selected stations from browser multiselect form
   for my $key (@{$c->req()->params()->names}) {
       print STDERR "$key:\n";
@@ -63,19 +73,21 @@ get '/' => sub ($c) {
 	          print STDERR "cnv file: " ;
 	          print STDERR ;
 	          print STDERR "\n" ;
-		  #	  push(@cnv_files_selected,$parameter);
+		  push(@cnv_files_selected,$_);
 	  }
       }elsif($key =~ /x_axis/){
           foreach (@$param_refs){
 	          print STDERR "x-axis: " ;
 	          print STDERR ;
 	          print STDERR "\n" ;
+		  $x_axis = $_;
 	  }
       }elsif($key =~ /y_axis/){
           foreach (@$param_refs){
 	          print STDERR "y-axis: " ;
 	          print STDERR ;
 	          print STDERR "\n" ;
+		  $y_axis = $_;
 	  }
       }else{
           print STDERR "ERROR" ;
@@ -84,13 +96,59 @@ get '/' => sub ($c) {
 
   };
 
+  my $index=0;
   foreach my $cnv_file (@cnv_files_selected) {
       my $cnv_fullpath_name = "${datadir}$cnv_file";
-      #get data for each cnv file selected by user
-      my $instrument = $x_axis;
+      $plots[$index] = Plot->new();
+      $plots[$index]->station($cnv_file);
+
+      #find x instrument associated with x-axis name given from user
+      my $x_axis_instrument = [];
+      foreach my $instrument (@instruments){
+          if($x_axis eq $instrument->name){
+              $x_axis_instrument = $instrument;
+              last;
+          }
+      }
+      print STDERR $x_axis_instrument->name;
+      $plots[$index]->x_instrument($x_axis_instrument);
+      
+      #get x data for instrument selected by user
       helper cnvdata => sub { state $cnvdata = CtdPlot::Model::getDataFromCNV->new };
-      @cnv_info = $c->cnvdata->get_data($cnv_fullpath_name,$instrument);
-      push(@all_cnv_info,\@cnv_info);
+      @cnv_info = $c->cnvdata->get_data($cnv_fullpath_name,$x_axis_instrument);
+      $plots[$index]->x_values(\@cnv_info);
+
+      
+      #find y instrument associated with x-axis name given from user
+      my $y_axis_instrument = [];
+      foreach my $instrument (@instruments){
+          if($y_axis eq $instrument->name){
+              $y_axis_instrument = $instrument;
+              last;
+          }
+      }
+      print STDERR $y_axis_instrument->name;
+      $plots[$index]->y_instrument($y_axis_instrument);
+
+      #get y data for instrument selected by user
+      helper cnvdata => sub { state $cnvdata = CtdPlot::Model::getDataFromCNV->new };
+      @cnv_info = $c->cnvdata->get_data($cnv_fullpath_name,$y_axis_instrument);
+      $plots[$index]->y_values(\@cnv_info);
+
+      $index++;
+  }
+
+  foreach my $inst (@plots){
+	  print STDERR "Station: ";
+	  print STDERR $inst->station;
+	  print STDERR "\n";
+	  print STDERR "Inst: ";
+	  print STDERR $inst->x_instrument->name;
+	  print STDERR "\n";
+	  print STDERR "Inst: ";
+	  print STDERR $inst->y_instrument->name;
+	  print STDERR "\n";
+          my $ary_ref = $inst->x_values;
   }
 
   #Send data to client 
